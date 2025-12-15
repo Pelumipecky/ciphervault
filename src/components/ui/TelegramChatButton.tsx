77c948408e6af3e219709b26f1f0e3086107d817
DEv rox <midlandcakess@gmail.com>
2025-12-15 20:48:25 +0100

    Add Telegram and WhatsApp chat button placeholders

---
+++ src/components/ui/TelegramChatButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TelegramChatButton: React.FC = () => (
+  <button>Telegram Chat</button>
+);
+
+export default TelegramChatButton;
---
+++ src/components/ui/WhatsAppChatButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WhatsAppChatButton: React.FC = () => (
+  <button>WhatsApp Chat</button>
+);
+
+export default WhatsAppChatButton;
