19d14de8fb567bd7e8f2b0f5e287e511444b8ff7
DEv rox <midlandcakess@gmail.com>
2026-01-12 01:14:48 +0100

    Update default sender to no-reply@ciphervault.online and fix .env typo

--- api/emailService.js
+++ api/emailService.js
@@ -4,7 +4,7 @@ import templates from './emailTemplates.js';
 // Configuration
 const MAILJET_API_KEY = process.env.MAILJET_API_KEY;
 const MAILJET_API_SECRET = process.env.MAILJET_API_SECRET;
-const EMAIL_FROM_ADDRESS = process.env.EMAIL_FROM || process.env.MAILJET_FROM_EMAIL || 'support@ciphervault.online';
+const EMAIL_FROM_ADDRESS = process.env.EMAIL_FROM || process.env.MAILJET_FROM_EMAIL || 'no-reply@ciphervault.online';
 const EMAIL_FROM_NAME = process.env.EMAIL_FROM_NAME || 'Cypher Vault';
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL || 'cyphervault6@gmail.com';
 
